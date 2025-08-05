fab1ee45fbe86901b40ce522055804e91bdc831b
Jordy Witteman <jordy.witteman@root3.nl>
2025-08-05 21:10:59 +0200

    Update ItemDouble.swift

--- src/Support/Views/ButtonTemplateViews/ItemDouble.swift
+++ src/Support/Views/ButtonTemplateViews/ItemDouble.swift
@@ -39,6 +39,9 @@ struct ItemDouble: View {
     // Get preferences or default values
     @ObservedObject var preferences = Preferences()
     
+    // Dark Mode detection
+    @Environment(\.colorScheme) var colorScheme
+    
     // Alert title options
     var alertTitle: String {
         switch linkType {
@@ -64,73 +67,145 @@ struct ItemDouble: View {
     }
     
     var body: some View {
-        
-        ZStack {
-            
-            HStack {
-                Ellipse()
-                    .foregroundColor(hoverView ? .primary : symbolColor)
-                    .overlay(
-                        Image(systemName: image)
-                            .foregroundColor(hoverView ? Color("hoverColor") : Color.white)
-                    )
-                    .frame(width: 26, height: 26)
-                    .padding(.leading, 10)
-                
-                VStack(alignment: .leading) {
-                    
-                    Text(hoverView && hoverEffectEnable ? secondTitle : title)
-                        .font(.system(.body, design: .rounded)).fontWeight(.medium)
-                        .lineLimit(2)
-                    
-                    Text(hoverView && hoverEffectEnable ? secondSubtitle : subtitle)
-                        .font(.system(.subheadline, design: .rounded))
-                        .lineLimit(2)
 
+        if #available(macOS 26, *) {
+            ZStack {
+                
+                HStack {
+                    Ellipse()
+                        .foregroundColor(.white)
+                        .overlay(
+                            Image(systemName: image)
+                                .foregroundColor(hoverView ? .primary : symbolColor)
+                                .font(.system(size: 18))
+                        )
+                        .frame(width: 36, height: 36)
+                        .padding(.leading, 10)
+                    
+                    VStack(alignment: .leading) {
+                        
+                        Text(hoverView && hoverEffectEnable ? secondTitle : title)
+                            .font(.system(.body, design: .default))
+                            .fontWeight(.medium)
+                            .foregroundStyle(.white)
+                            .lineLimit(2)
+                        
+                        Text(hoverView && hoverEffectEnable ? secondSubtitle : subtitle)
+                            .font(.system(.subheadline, design: .default))
+                            .foregroundStyle(.white.opacity(0.5))
+                            .lineLimit(2)
+
+                    }
+                    
+                    Spacer()
                 }
                 
-                Spacer()
-            }
-            
-            if notificationBadge != nil && notificationBadge! > 0 {
-                NotificationBadgeView(badgeCounter: notificationBadge!)
-            }
-            
-            if notificationBadgeBool ?? false {
-                NotificationBadgeTextView(badgeCounter: "!")
-            }
-        }
-        .frame(width: 176, height: 60)
-        .background(hoverView && hoverEffectEnable ? EffectsView(material: NSVisualEffectView.Material.windowBackground, blendingMode: NSVisualEffectView.BlendingMode.withinWindow) : EffectsView(material: NSVisualEffectView.Material.popover, blendingMode: NSVisualEffectView.BlendingMode.withinWindow))
-        .cornerRadius(10)
-        // Apply gray and black border in Dark Mode to better view the buttons like Control Center
-        .modifier(DarkModeBorder())
-        .shadow(color: Color.black.opacity(0.2), radius: 4, y: 2)
-        // FIXME: - Adjust when Jamf Connect Password Change can be triggered
-        // https://docs.jamf.com/jamf-connect/2.9.1/documentation/Jamf_Connect_URL_Scheme.html#ID-00005c31
-        .popover(isPresented: $showingAlert, arrowEdge: .leading) {
-            PopoverAlertView(uptimeAlert: $showingAlert, title: alertTitle, message: alertMessage)
-        }
-        .onHover() {
-            hover in self.hoverView = hover
-        }
-        .onTapGesture() {
-            if linkType == "App" {
-                openApp()
-            } else if linkType == "URL" {
-                openLink()
-            } else if linkType == "Command" {
-                runCommand()
+                if notificationBadge != nil && notificationBadge! > 0 {
+                    NotificationBadgeView(badgeCounter: notificationBadge!)
+                }
                 
-            // FIXME: - Asjust when Jamf Connect Password Change can be triggered
+                if notificationBadgeBool ?? false {
+                    NotificationBadgeTextView(badgeCounter: "!")
+                }
+            }
+            .frame(width: 176, height: 60)
+            .glassEffect(.clear.tint(colorScheme == .dark ? .clear : .secondary.opacity(0.6)))
+            // FIXME: - Adjust when Jamf Connect Password Change can be triggered
             // https://docs.jamf.com/jamf-connect/2.9.1/documentation/Jamf_Connect_URL_Scheme.html#ID-00005c31
-            } else if linkType == "JamfConnectPasswordChangeException" {
-                self.showingAlert.toggle()
-            } else if linkType == "KerberosSSOExtensionUnavailable" {
-                self.showingAlert.toggle()
-            } else {
-                self.showingAlert.toggle()
-                logger.error("Invalid Link Type: \(linkType!)")
+            .popover(isPresented: $showingAlert, arrowEdge: .leading) {
+                PopoverAlertView(uptimeAlert: $showingAlert, title: alertTitle, message: alertMessage)
+            }
+            .onHover() {
+                hover in self.hoverView = hover
+            }
+            .onTapGesture() {
+                if linkType == "App" {
+                    openApp()
+                } else if linkType == "URL" {
+                    openLink()
+                } else if linkType == "Command" {
+                    runCommand()
+                    
+                // FIXME: - Asjust when Jamf Connect Password Change can be triggered
+                // https://docs.jamf.com/jamf-connect/2.9.1/documentation/Jamf_Connect_URL_Scheme.html#ID-00005c31
+                } else if linkType == "JamfConnectPasswordChangeException" {
+                    self.showingAlert.toggle()
+                } else if linkType == "KerberosSSOExtensionUnavailable" {
+                    self.showingAlert.toggle()
+                } else {
+                    self.showingAlert.toggle()
+                    logger.error("Invalid Link Type: \(linkType!)")
+                }
+            }
+            
+        } else {
+            ZStack {
+                
+                HStack {
+                    Ellipse()
+                        .foregroundColor(hoverView ? .primary : symbolColor)
+                        .overlay(
+                            Image(systemName: image)
+                                .foregroundColor(hoverView ? Color("hoverColor") : Color.white)
+                        )
+                        .frame(width: 26, height: 26)
+                        .padding(.leading, 10)
+                    
+                    VStack(alignment: .leading) {
+                        
+                        Text(hoverView && hoverEffectEnable ? secondTitle : title)
+                            .font(.system(.body, design: .rounded)).fontWeight(.medium)
+                            .lineLimit(2)
+                        
+                        Text(hoverView && hoverEffectEnable ? secondSubtitle : subtitle)
+                            .font(.system(.subheadline, design: .rounded))
+                            .lineLimit(2)
+                        
+                    }
+                    
+                    Spacer()
+                }
+                
+                if notificationBadge != nil && notificationBadge! > 0 {
+                    NotificationBadgeView(badgeCounter: notificationBadge!)
+                }
+                
+                if notificationBadgeBool ?? false {
+                    NotificationBadgeTextView(badgeCounter: "!")
+                }
+            }
+            .frame(width: 176, height: 60)
+            .background(hoverView && hoverEffectEnable ? EffectsView(material: NSVisualEffectView.Material.windowBackground, blendingMode: NSVisualEffectView.BlendingMode.withinWindow) : EffectsView(material: NSVisualEffectView.Material.popover, blendingMode: NSVisualEffectView.BlendingMode.withinWindow))
+            .cornerRadius(10)
+            // Apply gray and black border in Dark Mode to better view the buttons like Control Center
+            .modifier(DarkModeBorder())
+            .shadow(color: Color.black.opacity(0.2), radius: 4, y: 2)
+            // FIXME: - Adjust when Jamf Connect Password Change can be triggered
+            // https://docs.jamf.com/jamf-connect/2.9.1/documentation/Jamf_Connect_URL_Scheme.html#ID-00005c31
+            .popover(isPresented: $showingAlert, arrowEdge: .leading) {
+                PopoverAlertView(uptimeAlert: $showingAlert, title: alertTitle, message: alertMessage)
+            }
+            .onHover() {
+                hover in self.hoverView = hover
+            }
+            .onTapGesture() {
+                if linkType == "App" {
+                    openApp()
+                } else if linkType == "URL" {
+                    openLink()
+                } else if linkType == "Command" {
+                    runCommand()
+                    
+                    // FIXME: - Asjust when Jamf Connect Password Change can be triggered
+                    // https://docs.jamf.com/jamf-connect/2.9.1/documentation/Jamf_Connect_URL_Scheme.html#ID-00005c31
+                } else if linkType == "JamfConnectPasswordChangeException" {
+                    self.showingAlert.toggle()
+                } else if linkType == "KerberosSSOExtensionUnavailable" {
+                    self.showingAlert.toggle()
+                } else {
+                    self.showingAlert.toggle()
+                    logger.error("Invalid Link Type: \(linkType!)")
+                }
             }
         }
     }
